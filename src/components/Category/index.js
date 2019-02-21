389822b03a4b027de4563d2ba80a1b18937946c2
David Boyne <davidboyne123@hotmail.co.uk>
2019-02-21 08:56:46 +0000

    Testing....

--- src/components/Category/index.js
+++ src/components/Category/index.js
@@ -1,29 +1,45 @@
 import React from 'react'
-import { navigate } from 'gatsby'
+import { navigate, StaticQuery } from 'gatsby'
 import iconToCategoryMap from '../../utils/icon-to-category'
 import genericIcon from '../../icons/circle.svg'
 import './styles.css'
-import { siteMetadata } from '../../../gatsby-config'
+// import { siteMetadata } from '../../../gatsby-config'
 
 const Category = ({ name } = {}) => {
-  const { subtitle } = siteMetadata.categories[name]
-  const icon = iconToCategoryMap[name] || genericIcon
-
   return (
-    <div className="category-container column is-one-third">
-      <div className="category" onClick={() => navigate(name)}>
-        <img src={icon} alt="icon" />
-        <h2 className=" has-text-centered">{name}</h2>
+    <StaticQuery
+      query={graphql`
+        query {
+          site {
+            siteMetadata {
+              description
+            }
+          }
+        }
+      `}
+      render={data => {
+        // const { subtitle } = data.site.siteMetadata.categories[name]
+        const icon = genericIcon
+        const subtitle = data.site.siteMetadata.description
 
-        <div className="category-content">
-          <div className="media">
-            <div className="media-content">
-              <p className="subtitle is-6">{subtitle}</p>
+        return (
+          <div className="category-container column is-one-third">
+            <div className="category" onClick={() => navigate(name)}>
+              <img src={icon} alt="icon" />
+              <h2 className=" has-text-centered">{name}</h2>
+
+              <div className="category-content">
+                <div className="media">
+                  <div className="media-content">
+                    <p className="subtitle is-6">{subtitle}</p>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
-    </div>
+        )
+      }}
+    />
   )
 }
 
